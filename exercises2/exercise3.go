40ac9b7e78ae0544d4efec3f4d34e6293d07bce1
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-01-21 10:11:12 -0500

    typed and untyped constants

---
+++ exercises2/exercise3.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a      = 24
+	b int8 = 29
+)
+
+func main() {
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+}
+
