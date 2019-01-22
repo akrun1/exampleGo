75a4bafd12cd7efb9e7b4331c5a0e12fc3f4b25d
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-01-22 12:18:53 -0500

    boolean edited

--- bool2.go
+++ bool2.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	fmt.Println(true && true)
-	fmt.Println(true && false)
+	fmt.Printf("true && true\t %v\n", true && true)
+	fmt.Printf("true && false\t %v\n", true && false)
 	fmt.Println(true || true)
 	fmt.Println(true || false)
 	fmt.Println(!true)
