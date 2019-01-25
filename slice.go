b91f6c855ae29eec33514692d2bc74756c35d231
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-01-25 13:33:02 -0500

    added composite slice

---
+++ slice.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// x := type{values} // composite literal
+	x := []int{4, 5, 7, 8, 12} // slice of int
+
+	fmt.Println(x)
+}
+// a SLICE allow to group together values of the same TYPE
