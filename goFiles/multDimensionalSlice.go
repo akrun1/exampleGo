a5676418f6ac9de565a0183c4d7ff904620db687
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-01-29 12:15:02 -0500

    added multi dimensional slice

---
+++ goFiles/multDimensionalSlice.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	jb := []string{"James", "Bond", "Chocolate", "Martini"}
+	fmt.Println(jb)
+	mp := []string{"Miss", "Moneypenny", "Strawberry", "Hazelnut"}
+	fmt.Println(mp)
+
+	xp := [][]string{jb, mp}
+	fmt.Println(xp)
+
+
+
+	y := [][]string{jb, mp}
+	fmt.Println(y)
+
+}
+
