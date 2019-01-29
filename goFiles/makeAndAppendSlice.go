7c427ca6be496087ea3d7572d97cec9c9198054f
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-01-28 19:27:26 -0500

    added make and append for slice

---
+++ goFiles/makeAndAppendSlice.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := make([]int, 10, 12)
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	x[0] = 42
+	x[9] = 999
+	fmt.Println(x)
+	x = append(x, 423)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 23, 42)
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+}
+
