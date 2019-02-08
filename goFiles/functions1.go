fb6a5e341d149d208c1613e7aaaf696855943001
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-02-08 13:53:33 -0500

    added function

---
+++ goFiles/functions1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	foo()
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { ... }
+
+func foo() {
+	fmt.Println("hello from foo")
+
+}
+
