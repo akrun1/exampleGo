2c7c5d12a1f58949e33d1d015d0922f35df024c8
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-02-11 12:36:18 -0500

    added unfurling a slice of int

---
+++ goFiles/unfurling.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum(xi...)
+	fmt.Println(x)
+}
+
+func sum(x ...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+	for i, v := range x {
+
+		sum += v
+		fmt.Println("for item in index position", i, "we are now adding", v, "to the total to give the total", sum)
+	}
+
+	return sum
+
+}
+
