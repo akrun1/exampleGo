ae337926a743482a393a631ea3c4e8796e1f97a7
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-02-12 16:59:05 -0500

    added anonymous function

---
+++ goFiles/anonymousFunc.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	foo()
+	func(){
+		fmt.Println("anonymous function ran")
+	}()
+
+	func(x int){
+		fmt.Println("The meaning of life: ", x)
+	}(20)
+
+}
+
+func foo() {
+	fmt.Println("foo ran")
+}
+
