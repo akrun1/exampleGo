3ddf2dd62ef11cd61e26324e89e1cf0dd11e5107
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-02-24 17:13:36 -0500

    added pointer

---
+++ goFiles/pointer4.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	length float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+
+	return s.length * s.length
+
+}
+func (c circle) area() float64 {
+
+	return math.Pi * c.radius * c.radius
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) {
+	fmt.Println("area is", s.area())
+}
+
+
+
+func main() {
+
+
+
+	c1 := circle{
+		radius: 12.0,
+	}
+
+	info(c1)
+	info(&c1)
+}
+
+
