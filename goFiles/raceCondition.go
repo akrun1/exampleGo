3e49d851821119603b6ccd9bb79b3d176d034500
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-03-25 16:49:46 -0400

    added race condition modified

--- goFiles/raceCondition.go
+++ goFiles/raceCondition.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
-	"time"
 )
 
 func main() {
 	fmt.Println("CPUs:", runtime.NumCPU())
 	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
 	counter := 0
+
 	const gs = 100
 	var wg sync.WaitGroup
 	wg.Add(gs)
@@ -18,20 +19,15 @@ func main() {
 	for i := 0; i < gs; i++ {
 		go func() {
 			v := counter
-			time.Sleep(time.Second)
+			// time.Sleep(time.Second)
 			runtime.Gosched()
 			v++
 			counter = v
 			wg.Done()
-
 		}()
 		fmt.Println("Goroutines:", runtime.NumGoroutine())
 	}
-
 	wg.Wait()
-
 	fmt.Println("Goroutines:", runtime.NumGoroutine())
 	fmt.Println("count:", counter)
-
 }
-
