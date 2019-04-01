c00e4f677373d94a3412f5f301b77a13eb219033
arun k <AeraTechnology@Aruns-MacBook-Pro.local>
2019-04-01 16:02:30 -0400

    added exercise3.go

--- exercise9/exercise3.go
+++ exercise9/exercise3.go
@@ -7,25 +7,27 @@ import (
 )
 
 func main() {
-	fmt.Println("CPUs:", runtime.NumCPU())
-	fmt.Println("Goroutines:", runtime.NumGoroutine())
 
+
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
 	counter := 0
 
 	const gs = 100
+
 	var wg sync.WaitGroup
 	wg.Add(gs)
-
 	for i := 0; i < gs; i++ {
 		go func() {
 			v := counter
-			// time.Sleep(time.Second)
 			runtime.Gosched()
 			v++
 			counter = v
 			wg.Done()
 		}()
-		fmt.Println("Goroutines:", runtime.NumGoroutine())
+		fmt.Println("Goroutines within loop:", runtime.NumGoroutine())
 	}
 	wg.Wait()
 	fmt.Println("Goroutines:", runtime.NumGoroutine())
